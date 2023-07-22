411467c3812beceeb40dd9b21a64cb4df723de7f
Emily <saeee210@gmail.com>
2023-07-22 23:42:36 +0900

    정확한 변수명으로 변경

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -5,31 +5,33 @@ export function statement(invoice, plays) {
   const format = new Intl.NumberFormat('en-US',
     { style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format;
 
+  function amountFor(aPerformance, play) {
+    let result = 0;
+    switch(play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = amountFor(perf, play);
 
-    function amountFor(perf, play) {
-      let thisAmount = 0;
-      switch(play.type) {
-        case 'tragedy':
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case 'comedy':
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${play.type}`);
-      }
-      return thisAmount;
-    }
+
 
     volumeCredits += Math.max(perf.audience - 30, 0);
     if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
