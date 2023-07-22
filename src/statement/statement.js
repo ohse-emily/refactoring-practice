5a9ca5dd84edf68df94fcb9f542435deb183a24d
Emily <saeee210@gmail.com>
2023-07-23 00:46:40 +0900

    playFor() 함수 data 인자에 맞게 변경

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -6,14 +6,20 @@ export function statement(invoice, plays) {
 
   function enrichPerformance(aPerformance) {
     const result = JSON.parse(JSON.stringify(aPerformance));
+    result.play = playFor(result);
+    console.log('result=', result);
     return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   result += `총액: ${usd(totalAmount())}\n`
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -43,17 +49,13 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 
   function amountFor(aPerformance) {
     let result = 0;
-    switch(playFor(aPerformance).type) {
+    switch(aPerformance.play.type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
