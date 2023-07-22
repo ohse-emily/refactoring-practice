e0f39a9f49471ce0eb52c2cf18b0a5fa90c7e1de
Emily <saeee210@gmail.com>
2023-07-22 23:36:49 +0900

    amount계산 부분 함수 추출하기

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -7,24 +7,28 @@ export function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
-    switch(play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${play.type}`);
+    function amountFor(perf, play) {
+      let thisAmount = 0;
+      switch(play.type) {
+        case 'tragedy':
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case 'comedy':
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error(`알 수 없는 장르: ${play.type}`);
+      }
+      return thisAmount;
     }
 
     volumeCredits += Math.max(perf.audience - 30, 0);
