cee57be7d509441cc11c3cd245a3c21b6d7e96d1
Emily <saeee210@gmail.com>
2023-07-23 23:24:21 +0900

    volumeCreditsFor 함수도 data 생성하는 곳으로 이동

--- src/statement/statement.js
+++ src/statement/statement.js
@@ -8,6 +8,7 @@ export function statement(invoice, plays) {
     const result = JSON.parse(JSON.stringify(aPerformance));
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,13 @@ export function statement(invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -58,7 +66,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -66,12 +74,5 @@ function renderPlainText(data, plays) {
   function usd(aNumber) {
     return new Intl.NumberFormat('en-US',
       { style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format(aNumber/100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
